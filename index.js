d77e6ef5ca2b02b2b1737aafeca1f2d08c47c7e2
Tatyana Kostromskaya <32135588+takost@users.noreply.github.com>
2023-01-18 12:13:15 +0000

    add action

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const argsToWrite = core.getInput('args');
+  console.log(argsToWrite);
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
